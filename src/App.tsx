add18ef7d8ae5aeca16f8b8dec844ef185dcdfbf
eugenehwng <eugenehw@andrew.cmu.edu>
2025-10-04 02:58:38 -0400

    fix frontend issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import './App.css';
 
 // Define the shape of a Tetris piece
